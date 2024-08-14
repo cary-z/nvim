8f00c3f5411f22304be40b8d91e3bace330a6b07
cary <812372447@qq.com>
2024-08-14 15:15:06 +0800

    feat: 配置cmp的快捷键

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,34 @@
+return {
+  "hrsh7th/nvim-cmp",
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      mapping = {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      },
+    })
+  end,
+}
